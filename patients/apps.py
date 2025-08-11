fdd87437528aba547b44bd3f7f70ec37f83f4314
tioRaffa <rafaelmuniz200@gmail.com>
2025-08-11 11:10:12 -0300

    Adiciona estrutura inicial para os aplicativos de consultas, médicos e pacientes no projeto Django

--- patients/apps.py
+++ patients/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SchedulingConfig(AppConfig):
+class PatientsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scheduling'
+    name = 'patients'
